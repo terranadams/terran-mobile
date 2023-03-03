02daaae6ad4ae0e24f29fee3759ae9df9c242291
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-03-03 15:22:49 -0700

    removed a 2 from the pokedex page lol

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.terranadams.terranmobile',
   appName: 'terran-mobile',
   webDir: 'www',
   bundledWebRuntime: false
 };
 
 export default config;
+// ionic capacitor add ios
+// ng build
+// ionic capacitor copy ios
+// ionic capacitor open ios
+
+// ionic capacitor run ios will combine the build and copy process
