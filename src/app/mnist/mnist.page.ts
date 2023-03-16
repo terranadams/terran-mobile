2cfd50949f2d1c55b21ab7e326dd22d0332ae1ad
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-03-16 16:44:39 -0600

    think i got er

--- src/app/mnist/mnist.page.ts
+++ src/app/mnist/mnist.page.ts
@@ -56,70 +56,78 @@ export class MnistPage implements OnInit {
 
   /// Clears the canvas and the information on the screen.
   public clear(): void {
-    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+    this.context.clearRect(
+      0,
+      0,
+      this.context.canvas.width,
+      this.context.canvas.height
+    );
     this.predicted = '';
   }
 
-   /// Captures events from the canvas.
+  /// Captures events from the canvas.
   /// Based on the type of the event (mousedown, mouseup, etc.) performs certain actions.
   /// In charge of drawing images on canvas and runing the model predictions once digit is drawn.
   private captureEvents(canvasHtmlElement: HTMLCanvasElement) {
     // Draw image.
     fromEvent(canvasHtmlElement, 'mousedown')
-    .pipe(
-    switchMap((e) => {
-        return fromEvent(canvasHtmlElement, 'mousemove')
-        .pipe(
+      .pipe(
+        switchMap((e) => {
+          return fromEvent(canvasHtmlElement, 'mousemove').pipe(
             takeUntil(fromEvent(canvasHtmlElement, 'mouseup')),
             takeUntil(fromEvent(canvasHtmlElement, 'mouseleave')),
             pairwise()
-        )
-    })
-    ).subscribe((res: any) => {
-      const clientRect = canvasHtmlElement.getBoundingClientRect();
+          );
+        })
+      )
+      .subscribe((res: any) => {
+        const clientRect = canvasHtmlElement.getBoundingClientRect();
 
-      const previousPosition = {
-        x: res[0].clientX - clientRect.left,
-        y: res[0].clientY - clientRect.top
-      };
+        const previousPosition = {
+          x: res[0].clientX - clientRect.left,
+          y: res[0].clientY - clientRect.top,
+        };
 
-      const currentPosition = {
-        x: res[1].clientX - clientRect.left,
-        y: res[1].clientY - clientRect.top
-      };
+        const currentPosition = {
+          x: res[1].clientX - clientRect.left,
+          y: res[1].clientY - clientRect.top,
+        };
 
-      this.drawOnCanvas(previousPosition, currentPosition);
+        this.drawOnCanvas(previousPosition, currentPosition);
 
-      // Drawing is finished, run the predictions
-      fromEvent(canvasHtmlElement, 'mouseup')
-      .subscribe( async () => {
-        const pred = await tf.tidy(() => {
+        // Drawing is finished, run the predictions
+        fromEvent(canvasHtmlElement, 'mouseup').subscribe(async () => {
+          const pred = await tf.tidy(() => {
+            // Convert the canvas pixels to
+            let image = this.getImage(canvasHtmlElement);
 
-          // Convert the canvas pixels to
-          let image = this.getImage(canvasHtmlElement)
+            // Make and format the predications
+            const output = this.model.predict(image) as any;
+            let predictions = Array.from(output.dataSync());
+            console.log(predictions);
 
-          // Make and format the predications
-          const output = this.model.predict(image) as any;
-          let predictions = Array.from(output.dataSync());
-          console.log(predictions);
-
-          // Write out the prediction.
-          for (let i = 0; i < predictions.length; i++) {
-            if (predictions[i] == "1") {
-              this.predicted = i.toString();
+            // Write out the prediction.
+            for (let i = 0; i < predictions.length; i++) {
+              if (predictions[i] == '1') {
+                this.predicted = i.toString();
+              }
             }
-          }
-          if (this.predicted == "") {
-            this.predicted = ":(";
-          }
+            if (this.predicted == '') {
+              this.predicted = 'Not sure :(';
+            }
+          });
         });
-      })
-  })
+      });
   }
 
   /// Handles drawing on the canvas.
-  private drawOnCanvas(previousPosition: { x: number, y: number }, currentPosition: { x: number, y: number }) {
-    if (!this.context) { return; }
+  private drawOnCanvas(
+    previousPosition: { x: number; y: number },
+    currentPosition: { x: number; y: number }
+  ) {
+    if (!this.context) {
+      return;
+    }
 
     this.context.beginPath();
 
@@ -128,46 +136,9 @@ export class MnistPage implements OnInit {
       this.context.lineTo(currentPosition.x, currentPosition.y);
       this.context.stroke();
     }
-}
+  }
 
-onTouchStart(event: TouchEvent) {
-  this.context.beginPath();
-  this.context.moveTo(event.touches[0].clientX, event.touches[0].clientY);
-}
-
-onTouchMove(event: TouchEvent) {
-  event.preventDefault();
-  this.context.lineTo(event.touches[0].clientX, event.touches[0].clientY);
-  this.context.stroke();
-  setTimeout(() => {
-    this.predict();
-  }, 500);
-}
-
-private async predict() {
-  const pred = await tf.tidy(() => {
-    // Convert the canvas pixels to
-    let image = this.getImage(this.canvas.nativeElement);
-
-    // Make and format the predictions
-    const output = this.model.predict(image) as any;
-    let predictions = Array.from(output.dataSync());
-    console.log(predictions);
-
-    // Write out the prediction.
-    for (let i = 0; i < predictions.length; i++) {
-      if (predictions[i] == "1") {
-        this.predicted = i.toString();
-      }
-    }
-    if (this.predicted == "") {
-      this.predicted = ":(";
-    }
-  });
-}
-
-private getImage(canvasHtmlElement: any)
-  {
+  private getImage(canvasHtmlElement: any) {
     this.context.drawImage(canvasHtmlElement, 0, 0, 28, 28);
     let imageData = this.context.getImageData(0, 0, 28, 28);
     let img = tf.browser.fromPixels(imageData, 1);
@@ -175,5 +146,47 @@ private getImage(canvasHtmlElement: any)
     imgtmp = tf.cast(imgtmp, 'float32');
 
     return imgtmp;
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.context.beginPath();
+    const rect = (this.canvas.nativeElement as HTMLCanvasElement).getBoundingClientRect();
+    const x = event.touches[0].clientX - rect.left;
+    const y = event.touches[0].clientY - rect.top;
+    this.context.moveTo(x, y);
+  }
+
+  onTouchMove(event: TouchEvent) {
+    event.preventDefault();
+    const rect = (this.canvas.nativeElement as HTMLCanvasElement).getBoundingClientRect();
+    const x = event.touches[0].clientX - rect.left;
+    const y = event.touches[0].clientY - rect.top;
+    this.context.lineTo(x, y);
+    this.context.stroke();
+    setTimeout(() => {
+      this.predict();
+    }, 2000);
+  }
+
+  private async predict() {
+    const pred = await tf.tidy(() => {
+      // Convert the canvas pixels to
+      let image = this.getImage(this.canvas.nativeElement);
+
+      // Make and format the predictions
+      const output = this.model.predict(image) as any;
+      let predictions = Array.from(output.dataSync());
+      console.log(predictions);
+
+      // Write out the prediction.
+      for (let i = 0; i < predictions.length; i++) {
+        if (predictions[i] == '1') {
+          this.predicted = i.toString();
+        }
+      }
+      if (this.predicted == '') {
+        this.predicted = 'Not sure :(';
+      }
+    });
   }
 }
