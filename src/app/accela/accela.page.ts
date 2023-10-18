bec3ce3a9e927ff00ba7eeb38df89db04b96991e
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-10-18 16:23:39 -0600

    console logging records now

--- src/app/accela/accela.page.ts
+++ src/app/accela/accela.page.ts
@@ -7,6 +7,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./accela.page.scss'],
 })
 export class AccelaPage implements OnInit {
+  private accessToken: string | undefined; // To store the access token
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -14,6 +16,7 @@ export class AccelaPage implements OnInit {
     this.getAccessToken();
   }
 
+  // Function to get the access token
   getAccessToken() {
     // Define the API endpoint to obtain the access token
     const apiUrl = 'https://auth.accela.com/oauth2/token';
@@ -35,14 +38,40 @@ export class AccelaPage implements OnInit {
       'Content-Type': 'application/x-www-form-urlencoded',
     });
 
-    // Send a POST request to the API
+    // Send a POST request to the API to get the access token
     this.http.post(apiUrl, this.encodeFormParams(body), { headers }).subscribe(
-      (response) => {
-        // Log the response to the console (you can handle it as needed)
-        console.log('Access Token Response:', response);
+      (response: any) => {
+        // Store the access token obtained from the response
+        this.accessToken = response.access_token;
+
+        // Now that we have the access token, make a GET request for records
+        console.log(`Access token: ${this.accessToken}`)
+        this.getRecords();
       },
       (error) => {
-        // Handle errors if the request fails
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  // Function to get records
+  getRecords() {
+    // Define the API endpoint to get records
+    const apiUrl = 'https://apis.accela.com/v4/records';
+
+    // Set the headers for the GET request
+    const headers = new HttpHeaders({
+      'Authorization': `${this.accessToken}`, // Include the access token in the Authorization header
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    });
+
+    // Send a GET request to the records API
+    this.http.get(apiUrl, { headers }).subscribe(
+      (response) => {
+        console.log('Records Response:', response); // Log the records response
+      },
+      (error) => {
         console.error('Error:', error);
       }
     );
@@ -53,5 +82,6 @@ export class AccelaPage implements OnInit {
     return Object.keys(params)
       .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
       .join('&');
-  }
+  } // The encodeFormParams function is a helper function that's used to convert an object containing key-value pairs into a URL-encoded string.
+  // This is necessary when making a POST request with a "x-www-form-urlencoded" content type, which is a common way to send data in HTTP requests, especially for form submissions.
 }
