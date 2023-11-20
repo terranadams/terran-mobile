7a888ccb41ea5f7e5b7d519f337347ca0c45ab76
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-11-19 17:40:36 -0700

    uuuuuuuugh fixed the git problem

--- src/app/pokedex/pokedex.service.ts
+++ src/app/pokedex/pokedex.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Pokemon } from './pokemon';
 
 @Injectable({
