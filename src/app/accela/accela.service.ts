208ae49d4827348f0718429db935d236783fe1e6
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-12-10 15:05:08 -0700

    getting environments back now

--- src/app/accela/accela.service.ts
+++ src/app/accela/accela.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
@@ -9,12 +9,19 @@ import { map } from 'rxjs/operators';
 export class AccelaService {
 
   private baseUrl = 'https://apis.accela.com/v4';
+  private appId = '637798588965730207';
 
   constructor(private http: HttpClient) { }
 
   getEnvironments(agencyName: string) {
     const url = `${this.baseUrl}/agencies/${agencyName}/environments`;
-    return this.http.get<{ result: { name: string; product: string; version: string }[]; status: number }>(url).pipe(
+
+    // Set the headers
+    const headers = new HttpHeaders({
+      'x-accela-appid': this.appId,
+    });
+
+    return this.http.get<{ result: { name: string; product: string; version: string }[]; status: number }>(url, { headers }).pipe(
       map((response) => response.result.map((env) => env.name)) // Map out the name of each env to an array of strings
     );
   }
